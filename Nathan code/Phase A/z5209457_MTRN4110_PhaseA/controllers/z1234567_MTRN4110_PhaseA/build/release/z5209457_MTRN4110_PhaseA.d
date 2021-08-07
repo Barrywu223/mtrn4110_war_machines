4d85b44aeb703cf5be20da4d235c51a661f52c69
Nathan Long <ebzonman@gmail.com>
2021-08-07 13:15:28 +1000

    Merging Nathans code

---
+++ Nathan code/Phase A/z5209457_MTRN4110_PhaseA/controllers/z1234567_MTRN4110_PhaseA/build/release/z5209457_MTRN4110_PhaseA.d
@@ -0,0 +1,10 @@
+build/release/z5209457_MTRN4110_PhaseA.o: z5209457_MTRN4110_PhaseA.cpp \
+ CustomRobot.h \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/Stuart/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Users\Stuart\AppData\Local\Programs\Webots/include/controller/cpp/webots/Compass.hpp \
+ PathRobot.h
