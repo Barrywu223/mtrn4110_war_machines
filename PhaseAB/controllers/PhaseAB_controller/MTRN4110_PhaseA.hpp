c62543f87e2bef7ae59fac8c5965763beac0537f
Nathan Long <ebzonman@gmail.com>
2021-08-11 19:08:16 +1000

    working A&B with classes, still messy

--- PhaseAB/controllers/PhaseAB_controller/MTRN4110_PhaseA.hpp
+++ PhaseAB/controllers/PhaseAB_controller/MTRN4110_PhaseA.hpp
@@ -1,25 +1,7 @@
 #ifndef MTRN4110_PHASEA_HPP
 #define MTRN4110_PHASEA_HPP
 
-#include <fstream>
-#include <string>
-#include <iomanip>
-#include <map>
-#include <webots/Robot.hpp>
-#include <webots/Motor.hpp>
-#include <webots/DistanceSensor.hpp>
-#include <webots/Compass.hpp>
 
-// All the webots classes are defined in the "webots" namespace
-using namespace webots;
-using namespace std;
-const double Pi = 3.1415926;
-const double distanceThreshold = 850;
-const double MaxRotaionSpeed = 6.28 * 0.75; // Max. rotation speed = 6.28 rad/s
-const string MOTION_PLAN_FILE_NAME = "../../MotionPlan.txt";
-const string MOTION_EXUCUTION_FILE_NAME = "../../MotionExecution.csv";
-const string PRELIX = "[z5211399_MTRN4110_PhaseA] ";
-const int TIME_STEP = 64;
 
 struct ds_result
 {
--- PhaseAB/controllers/PhaseAB_controller/main.cpp
+++ PhaseAB/controllers/PhaseAB_controller/main.cpp
@@ -1,11 +1,9 @@
-#include "MTRN4110_PhaseA.hpp"
+#include "CustomRobot.hpp"
+#include "PathRobot.hpp"
 #include "PathPlanner.hpp"
 
-using namespace webots;
-using namespace std;
-
-unsigned stepIndex = 3;
-Message message;
+// unsigned stepIndex = 3;
+// Message message;
 
 int main(int argc, char **argv) {
   
@@ -14,72 +12,80 @@ int main(int argc, char **argv) {
   planner->run();
   delete planner;
   
-  // creat scv file
-  ofstream myFile(MOTION_EXUCUTION_FILE_NAME);
-  myFile << "Step,Row,Column,Heading,Left Wall,Front Wall,Right Wall" 
-  << endl;
-  
-  // create the Robot instance.  
-  Robot *robot = new Robot();
-  
-  // initialize motor, compass and distance sensors
-  Motor *leftMotor = robot->getMotor("left wheel motor");
-  Motor *rightMotor = robot->getMotor("right wheel motor");
-  Compass *compass = robot->getCompass("compass");
-  compass->enable(TIME_STEP);
-  DistanceSensor *ds[3];
-  char dsNames[3][10] = {"ds_right","ds_front","ds_left"};
-  for(int i = 0; i < 3; i++){
-    ds[i] = robot->getDistanceSensor(dsNames[i]);
-    ds[i]->enable(TIME_STEP);
-  }  
-  
-  // read the content form MotionPlan.txt
-  string textContent = readMotionPlan();
-  message.row = textContent[0] - 48;
-  message.column = textContent[1] - 48;
-  message.heading = textContent[2];
-  
-  while (robot->step(TIME_STEP) != -1) {
-    leftMotor->setPosition(INFINITY);
-    rightMotor->setPosition(INFINITY);
-    auto totalStep = textContent.length();
-    for(stepIndex = 3; stepIndex < totalStep; ++stepIndex){;
-      char command = textContent.at(stepIndex);
-      obstacleDetection(ds,message);
-      wirteMotionExectuion(myFile, message);
-      printMessage(message);
-      updateMessage(message, command);
-      if (command == 'F'){
-        go_forward(leftMotor, rightMotor);
-        robot->step(1700);
-      }
-      else if (command == 'L'){
-        turn_left(leftMotor, rightMotor);
-        robot->step(1400);
-      }
-      else if (command == 'R'){
-        turn_right(leftMotor, rightMotor);
-        robot->step(1400);
-      }
-      // rotational correctoin by compass      
-      angleCorrection(compass, message.heading, leftMotor, rightMotor, robot);
-      // linear correction if a wall is ahead
-      if (ds[1]->getValue() < distanceThreshold){
-        linearCorrection(ds[1], leftMotor, rightMotor, robot);
-      }
-    }
-    obstacleDetection(ds, message);
-    wirteMotionExectuion(myFile, message);
-    printMessage(message);
-    break;
-  };
+  // create the webots base Robot
+  Robot* robot = new Robot();
+  // Step world so data is available from sensors
+  // robot->step(2*robot->getBasicTimeStep()); 
+  // Create wrapper classes
+  PathRobot* pr = new PathRobot(robot);
+
+  // Main function
+  pr->followMP();
 
 
-  // Enter here exit cleanup code.
-  cout << PRELIX << "Motion plan executed!" << endl;
-  turn_off(leftMotor, rightMotor);
-  delete robot;
-  myFile.close();
   return 0;
+
+  // // create the Robot instance.  
+  // Robot *robot = new Robot();
+  
+  // // initialize motor, compass and distance sensors
+  // Motor *leftMotor = robot->getMotor("left wheel motor");
+  // Motor *rightMotor = robot->getMotor("right wheel motor");
+  // Compass *compass = robot->getCompass("compass");
+  // compass->enable(TIME_STEP);
+  // DistanceSensor *ds[3];
+  // char dsNames[3][10] = {"ds_right","ds_front","ds_left"};
+  // for(int i = 0; i < 3; i++){
+  //   ds[i] = robot->getDistanceSensor(dsNames[i]);
+  //   ds[i]->enable(TIME_STEP);
+  // }  
+  
+  // // read the content form MotionPlan.txt
+  // string textContent = readMotionPlan();
+  // message.row = textContent[0] - 48;
+  // message.column = textContent[1] - 48;
+  // message.heading = textContent[2];
+  
+  // while (robot->step(TIME_STEP) != -1) {
+  //   leftMotor->setPosition(INFINITY);
+  //   rightMotor->setPosition(INFINITY);
+  //   auto totalStep = textContent.length();
+  //   for(stepIndex = 3; stepIndex < totalStep; ++stepIndex){;
+  //     char command = textContent.at(stepIndex);
+  //     obstacleDetection(ds,message);
+  //     wirteMotionExectuion(myFile, message);
+  //     printMessage(message);
+  //     updateMessage(message, command);
+  //     if (command == 'F'){
+  //       go_forward(leftMotor, rightMotor);
+  //       robot->step(1700);
+  //     }
+  //     else if (command == 'L'){
+  //       turn_left(leftMotor, rightMotor);
+  //       robot->step(1400);
+  //     }
+  //     else if (command == 'R'){
+  //       turn_right(leftMotor, rightMotor);
+  //       robot->step(1400);
+  //     }
+  //     // rotational correctoin by compass      
+  //     angleCorrection(compass, message.heading, leftMotor, rightMotor, robot);
+  //     // linear correction if a wall is ahead
+  //     if (ds[1]->getValue() < distanceThreshold){
+  //       linearCorrection(ds[1], leftMotor, rightMotor, robot);
+  //     }
+  //   }
+  //   obstacleDetection(ds, message);
+  //   wirteMotionExectuion(myFile, message);
+  //   printMessage(message);
+  //   break;
+  // };
+
+
+  // // Enter here exit cleanup code.
+  // cout << PRELIX << "Motion plan executed!" << endl;
+  // turn_off(leftMotor, rightMotor);
+  // delete robot;
+  // myFile.close();
+  // return 0;
 }
